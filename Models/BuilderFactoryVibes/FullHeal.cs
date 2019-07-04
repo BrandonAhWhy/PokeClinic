107d097a9b25c437babc7f5d5970b133350936dc
vieirad02 <dario.vieira@vcontractor.co.za>
2019-07-04 08:15:24 +0200

    {{Implemented}} Builder/FactoryPattern for items

--- Models/BuilderFactoryVibes/FullHeal.cs
+++ Models/BuilderFactoryVibes/FullHeal.cs
@@ -1,28 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using System;
+using PokeClinic.Models.IRestoreBuilder;
+using RestoreItem;
 
-namespace PokeClinic
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-            runBuilderFactoryVibes();
-        }
+namespace PokeClinic.Models {
+    public class FullHeal {
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-        
-        private void runBuilderFactoryVibes(){
+        public static void main(){
             IRestoreBuilder healAll = new PotionRestorerBuilder ();
             IRestoreBuilder healFire = new OranRestorerBuilder ();
             IRestoreBuilder healWater = new SitrusRestorerBuilder ();
@@ -43,5 +26,6 @@ namespace PokeClinic
             Console.WriteLine ("Fire:", itemFire);
             Console.WriteLine ("Water:", itemWater);
         }
+
     }
 }
