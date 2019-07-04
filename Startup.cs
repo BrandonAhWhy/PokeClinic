fd2913a9706c4e978a460f02c64a680ffb8b3e6a
Mikhail Padayachee <mikhail@bbd.co.za>
2019-07-04 13:20:57 +0200

    added use statement for swagger

--- Startup.cs
+++ Startup.cs
@@ -52,6 +52,7 @@ namespace PokeClinic
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
