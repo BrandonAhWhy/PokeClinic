ca680eb6c3b7275bb595c8914639ae4766572d46
vieirad02 <dario.vieira@vcontractor.co.za>
2019-07-04 22:44:08 +0200

    Added Endpoint and SQL for next available booking

--- Controllers/ApiV1/TreatmentController.cs
+++ Controllers/ApiV1/TreatmentController.cs
@@ -9,11 +9,18 @@ namespace PokeClinic.Controllers.ApiV1
     [Route("api/v1/treatment")]
     [ApiController]
     public class TreatmentController: Controller{
+        
+        public static string nextAptSQL = "SELECT day, COUNT(id) AS numBookings FROM schedule GROUP BY schedule.day HAVING numBookings < 3 ORDER BY schedule.day ASC;";
 
         [HttpGet("{type}")]
         public ActionResult getAvailability(string type){
             TreatmentReturn treatment = TreatmentHandler.getAvailibleTreatment(type);
             return Json(treatment);
+        }
+
+        [HttpGet("next/available")]
+        public ActionResult nextAppt(){
+            return Json(TreatmentHandler.scheduleSqlRunner(nextAptSQL));
         }
     }
 }
